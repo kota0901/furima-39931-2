a418d4c75e7b30dca14bd4044b9c73923657ed0f
Kota Ishida <kota.ishida@di-v.co.jp>
2024-05-03 13:56:17 +0900

    items_controllerファイル「authenticate_user!」メソッドの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   def index
     @items = Item.includes(:user).order("created_at DESC")
 
