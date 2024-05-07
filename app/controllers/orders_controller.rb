15fb756c095fca95c9ec26cb5de3a70359d45a85
Kota Ishida <kota.ishida@di-v.co.jp>
2024-05-07 16:07:54 +0900

    ストロングパラメータの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,4 +4,17 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order = PayForm.new
   end
+
+  private
+
+def order_params
+  params.require(:pay_form).permit(
+    :postal_code,
+    :region_id,
+    :municipalities,
+    :banchi,
+    :building_name,
+    :number
+    ).merge(user_id: current_user.id,item_id: params[:item_id])
+end
 end
